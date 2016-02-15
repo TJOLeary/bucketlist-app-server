70fea011c9376d37139c434f54fba11c7132bdd5
Tom <thomas.oleary@hotmail.co.uk>
2016-02-15 16:24:03 +0000

    Updated db connection

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'mongodb://ec2-52-49-222-66.eu-west-1.compute.amazonaws.com:27017/local'
+  'url' : 'mongodb://ec2-52-30-112-75.eu-west-1.compute.amazonaws.com:27017/local'
 }
